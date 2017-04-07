26366db8ea434df1ef39f6f18a55f822dcd5dbb2
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-07 11:13:52 -0400

    Add project files.

---
+++ chrome-dev-tools-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chrome_dev_tools_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
