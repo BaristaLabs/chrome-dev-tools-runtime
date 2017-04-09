fefd03dfa157e388cf4d19e744449b9ced005850
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-09 10:22:28 -0400

    Remove using system.io

--- ChromeDevToolsCLI/Program.cs
+++ ChromeDevToolsCLI/Program.cs
@@ -5,7 +5,6 @@
     using Newtonsoft.Json.Linq;
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
